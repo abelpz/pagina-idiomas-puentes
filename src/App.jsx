8347b545ca1d40b1a64ccf7ccb70eda999f2d71c
Abel Pérez <abelper54@gmail.com>
2024-08-21 16:48:14 -0400

    Cambio de titulo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Pagina de prueba de Idiomas Puentes</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
